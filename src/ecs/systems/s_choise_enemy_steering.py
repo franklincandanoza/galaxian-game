a90410ba6c2cb6c86a0b37233eb1abaa04edd28b
David Hidalgo <d.hidalgo@uniandes.edu.co>
2023-05-21 03:25:44 -0500

    se agrega sonido al lanzarse el enemigo

--- src/ecs/systems/s_choise_enemy_steering.py
+++ src/ecs/systems/s_choise_enemy_steering.py
@@ -11,18 +11,21 @@ from src.ecs.components.c_steering_state import CSteeringState
 from src.ecs.components.c_transform import CTransform
 from src.ecs.components.c_velocity import CVelocity
 from src.ecs.components.tags.c_tag_grouped_enemy import CTagGroupEnemy
+from src.engine.service_locator import ServiceLocator
 def system_choose_enemy_steering(world:esper.World,enemy_steering_info:dict, delta_time:float):
 
     component = world.get_component(CEnemyChooseSteering)
  
     for _, (c_cs) in component: 
+        if world.get_components(CSteeringState):
+            continue
         c_cs.current_time += delta_time
         if c_cs.current_time>=c_cs.time_steering:
             c_cs.current_time = 0
             number_of_enemies = random.randint(c_cs.enemies_to_steering_min,c_cs.enemies_to_steering_max)
             components_enemy = world.get_component(CTagGroupEnemy)
             component_enemy_list = [enemy_entity for enemy_entity,_ in components_enemy]
-            
+                           
             if not component_enemy_list:
                 break
             if len(component_enemy_list)<number_of_enemies:
@@ -34,4 +37,6 @@ def system_choose_enemy_steering(world:esper.World,enemy_steering_info:dict, del
                 world.add_component(entity,CSteering(enemy_steering_info))
                 world.add_component(entity,CSteeringState(c_t.pos.copy()))
                 world.add_component(entity,CEnemySteeringFire(enemy_steering_info["fire"]))
+                ServiceLocator.sounds_service.play(enemy_steering_info["enemy_launch"])
+                
  
